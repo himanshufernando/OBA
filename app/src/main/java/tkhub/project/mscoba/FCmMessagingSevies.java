e3ed99a625c61422b5ebd802231302828b6eb575
Himanshu <himanshu.fernando@gmail.com>
2017-01-09 15:13:24 +0530

    2017-01-09 1.0

--- app/src/main/java/tkhub/project/mscoba/FCmMessagingSevies.java
+++ app/src/main/java/tkhub/project/mscoba/FCmMessagingSevies.java
@@ -4,10 +4,12 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -16,7 +18,11 @@ import com.google.firebase.messaging.RemoteMessage;
 import java.sql.SQLOutput;
 import java.util.Map;
 
+import tkhub.project.mscoba.Layout.Event;
+import tkhub.project.mscoba.Layout.EventFragment;
+import tkhub.project.mscoba.Layout.Gallery;
 import tkhub.project.mscoba.Layout.News;
+import tkhub.project.mscoba.Layout.Newslatters;
 import tkhub.project.mscoba.Layout.Splash;
 
 
@@ -27,23 +33,22 @@ import tkhub.project.mscoba.Layout.Splash;
 public class FCmMessagingSevies extends FirebaseMessagingService {
 
 
-    private static final String TAG = "kkkkkkkkkk";
+
     String key;
     String value;
+
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        String titel =remoteMessage.getNotification().getTitle();
-        String message =remoteMessage.getNotification().getBody();
+        String titel = remoteMessage.getNotification().getTitle();
+        String message = remoteMessage.getNotification().getBody();
 
-        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
         for (Map.Entry<String, String> entry : remoteMessage.getData().entrySet()) {
-             key = entry.getKey();
-             value = entry.getValue();
+            key = entry.getKey();
+            value = entry.getValue();
 
         }
-
-
 
 
         NotificationCompat.Builder mBuilder =
@@ -53,25 +58,43 @@ public class FCmMessagingSevies extends FirebaseMessagingService {
                         .setAutoCancel(true)
                         .setSound(defaultSoundUri)
                         .setContentText(message);
-        Intent resultIntent ;
+        Intent resultIntent = null;
 
 
 
+       switch (value) {
+            case "1":
+               resultIntent = new Intent(this, News.class);
+                break;
+            case "2":
+               resultIntent = new Intent(this, Event.class);
+                break;
+            case "3":
+               resultIntent = new Intent(this, Newslatters.class);
+                break;
+            case "4":
+                resultIntent = new Intent(this, Gallery.class);
+                break;
+            case "5":
+                resultIntent = new Intent(Intent.ACTION_VIEW);
+                resultIntent.setData(Uri.parse("market://details?id=tkhub.project.mscoba"));
 
-         resultIntent = new Intent(this, News.class);
+        }
+
+
 
 
         TaskStackBuilder stackBuilder = null;
         PendingIntent resultPendingIntent = null;
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
             stackBuilder = TaskStackBuilder.create(this);
-            stackBuilder.addParentStack(News.class);
+          //  stackBuilder.addParentStack(News.class);
 
             stackBuilder.addNextIntent(resultIntent);
             resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-        }else {
+        } else {
 
-            resultPendingIntent= PendingIntent.getActivity(this, 0, resultIntent, PendingIntent.FLAG_ONE_SHOT);
+            resultPendingIntent = PendingIntent.getActivity(this, 0, resultIntent, PendingIntent.FLAG_ONE_SHOT);
 
         }
 
@@ -80,4 +103,5 @@ public class FCmMessagingSevies extends FirebaseMessagingService {
         mNotificationManager.notify(0, mBuilder.build());
         super.onMessageReceived(remoteMessage);
     }
+
 }
